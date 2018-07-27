7042017fa152a5fb5f1a7d877f55b4b8f07f11d2
Alex <alex@picotory.com>
2018-07-27 16:48:01 +0100

    Update titles

--- app/tpl/create-group.php
+++ app/tpl/create-group.php
@@ -57,7 +57,7 @@
         </div>
 
         <div class="siimple-jumbotron siimple-jumbotron--extra-large siimple-jumbotron--light">
-            <div class="siimple-jumbotron-title">Welcome!</div>
+            <div class="siimple-jumbotron-title">Create group</div>
             <div class="siimple-jumbotron-detail">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  
             </div>
