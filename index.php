25cb4f10d8614e35e327043651e1c2e83eb9b33b
Alex <alex@picotory.com>
2018-07-31 13:55:34 +0100

    Include lang, temp

--- index.php
+++ index.php
@@ -3,9 +3,10 @@
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
 
-    include 'app/viro.app.php';
+    $ViroLang = 'en';
 
-    $vApp = new Viro();
+    include 'app/viro.app.php';
+    include 'app/lang/' . $ViroLang . '.php';
 
     # Simple templating
     if(Viro::LoggedIn()) {
