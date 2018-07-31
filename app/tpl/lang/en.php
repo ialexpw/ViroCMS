791b452d1be93f449bd8a3214926201fa2d09f7f
Alex <alex@picotory.com>
2018-07-31 13:42:30 +0100

    For future - multi lang

---
+++ app/tpl/lang/en.php
@@ -0,0 +1,36 @@
+<?php
+    $l = array(
+        # Menu
+        'Profile'   => 'Profile',
+        'Logout'    => 'Logout',
+
+        # Titles
+        'Dashboard' => 'Dashboard',
+        'Articles'  => 'Articles',
+        'ContGrp'   => 'Content - Groups',
+        'CretGrp'    => 'Create group',
+        'ContZne'   => 'Content - Zones',
+        'CretZne'    => 'Create zone',
+        'ContEdt'   => 'Edit zone',
+        'Login'     => 'Login',
+        'Tools'     => 'Tools',
+        'Users'     => 'Users',
+
+        # Page info lines
+        'DashInfo'  => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        'ArtcInfo'  => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        '' => '',
+        '' => '',
+        '' => '',
+        '' => '',
+        '' => '',
+        '' => '',
+        '' => '',
+        '' => '',
+        '' => '',
+        '' => '',
+        '' => '',
+        '' => '',
+        '' => '',
+    );
+?>
