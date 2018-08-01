2c3166706090edf75c74a8defbf37201c5e2ac72
Alex <alex@picotory.com>
2018-08-01 15:38:08 +0100

    Add translations and header

--- app/tpl/dashboard.php
+++ app/tpl/dashboard.php
@@ -1,4 +1,17 @@
 <?php
+    /**
+     * dashboard.php
+     *
+     * Main "landing" page
+     *
+     * @package    ViroCMS
+     * @author     Alex White (https://github.com/ialexpw)
+     * @copyright  2018 ViroCMS
+     * @license    https://github.com/ialexpw/ViroCMS/blob/master/LICENSE  MIT License
+     * @link       https://viro.app
+     */
+
+    global $l;
     $Connect = Viro::Connect();
 
     # SELECT Groups
@@ -12,7 +25,7 @@
         <meta name="robots" content="index, follow">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
-        <title>ViroCMS - Dashboard</title>
+        <title>ViroCMS - <?php Viro::Translate('Dashboard', $l); ?></title>
 
         <!-- Styles -->
         <link rel="stylesheet" href="app/tpl/css/siimple.css">
@@ -24,15 +37,15 @@
         <div class="siimple-navbar siimple-navbar--extra-large siimple-navbar--dark">
             <div class="siimple-navbar-title">ViroCMS</div>
             <div class="siimple--float-right">
-                <a href="?page=profile"><div class="siimple-navbar-item">Profile</div></a>
-                <a href="?logout"><div class="siimple-navbar-item">Logout</div></a>
+                <a href="?page=profile"><div class="siimple-navbar-item"><?php Viro::Translate('Profile', $l); ?></div></a>
+                <a href="?logout"><div class="siimple-navbar-item"><?php Viro::Translate('Logout', $l); ?></div></a>
             </div>
         </div>
 
         <div class="siimple-jumbotron siimple-jumbotron--extra-large siimple-jumbotron--light">
-            <div class="siimple-jumbotron-title">Dashboard</div>
+            <div class="siimple-jumbotron-title"><?php Viro::Translate('Dashboard', $l); ?></div>
             <div class="siimple-jumbotron-detail">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.  
+                <?php Viro::Translate('DashI', $l); ?>
             </div>
         </div>
 
@@ -43,27 +56,27 @@
                         <div class="siimple-list siimple-list--hover">
                             <div class="siimple-list-item">
                                 <a href="?page=dashboard">
-                                    <div class="siimple-list-title">Dashboard <div class="siimple--float-right"><i class="fas fa-home"></i></div></div>
+                                    <div class="siimple-list-title"><?php Viro::Translate('Dashboard', $l); ?> <div class="siimple--float-right"><i class="fas fa-home"></i></div></div>
                                 </a>
                             </div>
                             <div class="siimple-list-item">
                                 <a href="?page=content">
-                                    <div class="siimple-list-title">Content <div class="siimple--float-right"><i class="far fa-edit"></i></div></div>
+                                    <div class="siimple-list-title"><?php Viro::Translate('Content', $l); ?> <div class="siimple--float-right"><i class="far fa-edit"></i></div></div>
                                 </a>
                             </div>
                             <div class="siimple-list-item">
                                 <a href="?page=articles">
-                                    <div class="siimple-list-title">Articles <div class="siimple--float-right"><i class="far fa-newspaper"></i></div></div>
+                                    <div class="siimple-list-title"><?php Viro::Translate('Articles', $l); ?> <div class="siimple--float-right"><i class="far fa-newspaper"></i></div></div>
                                 </a>
                             </div>
                             <div class="siimple-list-item">
                                 <a href="?page=users">
-                                    <div class="siimple-list-title">User management <div class="siimple--float-right"><i class="far fa-user-circle"></i></div></div>
+                                    <div class="siimple-list-title"><?php Viro::Translate('UserMgn', $l); ?> <div class="siimple--float-right"><i class="far fa-user-circle"></i></div></div>
                                 </a>
                             </div>
                             <div class="siimple-list-item">
                                 <a href="?page=tools">
-                                    <div class="siimple-list-title">Backup &amp; restore <div class="siimple--float-right"><i class="fas fa-sync-alt"></i></div></div>
+                                    <div class="siimple-list-title"><?php Viro::Translate('Backup', $l); ?> <div class="siimple--float-right"><i class="fas fa-sync-alt"></i></div></div>
                                 </a>
                             </div>
                         </div>
@@ -72,11 +85,19 @@
                     <div class="siimple-grid-col siimple-grid-col--9">
                         <!-- Breadcrumb menu -->
                         <div class="siimple-breadcrumb">
-                            <div class="siimple-breadcrumb-item">Dashboard</div>
+                            <div class="siimple-breadcrumb-item"><?php Viro::Translate('Dashboard', $l); ?></div>
                         </div>
 
                         <!-- Break line -->
                         <div class="siimple-rule"></div>
+
+                        <?php
+                            if(Viro::CheckUpdate()) {
+                                echo '<div class="siimple-alert siimple-alert--primary">';
+                                echo '<i class="fas fa-info"></i> <b>Information</b> - There is a newer release of ViroCMS, ensure to update when you get a chance!';
+                                echo '</div>';
+                            }
+                        ?>
 
                         <div class="siimple-card" style="max-width:300px;">
                             <div class="siimple-card-body">
