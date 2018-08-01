94088c37b18ee18b7b9cd1bbfefde47790e8071f
Alex <alex@picotory.com>
2018-08-01 15:37:55 +0100

    Remove debug from installer

--- install.php
+++ install.php
@@ -10,10 +10,6 @@
      * @license    https://github.com/ialexpw/ViroCMS/blob/master/LICENSE  MIT License
      * @link       https://viro.app
      */
-
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
     
     include 'app/viro.app.php';
 
