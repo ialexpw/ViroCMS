c056953ebd8339e4b0f86110ddbf1ccba505d5b5
Alex <alex@picotory.com>
2018-08-02 10:43:47 +0100

    add comments

--- app/viro.app.php
+++ app/viro.app.php
@@ -16,19 +16,24 @@
 		session_start();
     }
 
-    # ViroCMS Class
+    /**
+     * Viro Class
+     */
     class Viro {
-        /*
 
-        */
+        /**
+         * Viro::Connect()
+         * Connect to the SQLite database and return the database query
+         */
         public static function Connect() {
             $db = new SQLite3('app/db/viro.db', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
             return $db;
         }
 
-        /*
-
-        */
+        /**
+         * Viro::InstallDatabase()
+         * Install the database tables needed for ViroCMS
+         */
         public static function InstallDatabase() {
             $db = new SQLite3('app/db/viro.db', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
 
@@ -97,83 +102,13 @@
                 FOREIGN KEY(u_id) REFERENCES users(id) ON UPDATE CASCADE ON DELETE CASCADE
             )');
 
-
-
-/*
-            # Users table
-            $db->query('CREATE TABLE IF NOT EXISTS users (
-                id integer PRIMARY KEY AUTOINCREMENT,
-                username varchar,
-                email varchar,
-                password varchar,
-                read varchar,
-                write varchar,
-                users varchar,
-                tools varchar,
-                last_login varchar,
-                active integer
-            )');
-
-            # Groups table
-            $db->query('CREATE TABLE IF NOT EXISTS groups (
-                id integer PRIMARY KEY AUTOINCREMENT,
-                g_name varchar,
-                g_slug varchar,
-                g_hash varchar,
-                g_owner integer,
-                created varchar
-            )');
-
-            # Zones table
-            $db->query('CREATE TABLE IF NOT EXISTS zones (
-                id integer PRIMARY KEY AUTOINCREMENT,
-                z_name varchar,
-                z_slug varchar,
-                z_hash varchar,
-                g_hash varchar,
-                z_owner integer,
-                created varchar
-            )');
-
-            # Content table
-            $db->query('CREATE TABLE IF NOT EXISTS content (
-                id integer PRIMARY KEY AUTOINCREMENT,
-                content varchar,
-                c_hash varchar,
-                z_hash varchar,
-                g_hash varchar,
-                created varchar,
-                edit_by varchar,
-                updated varchar
-            )');
-
-            # Articles table
-            $db->query('CREATE TABLE IF NOT EXISTS articles (
-                id integer PRIMARY KEY AUTOINCREMENT,
-                title varchar,
-                author integer,
-                content varchar,
-                a_hash varchar,
-                created varchar,
-                updated varchar,
-                published integer
-            )');
-
-            # Backups table
-            $db->query('CREATE TABLE IF NOT EXISTS backups (
-                id integer PRIMARY KEY AUTOINCREMENT,
-                title varchar,
-                author integer,
-                created varchar
-            )');
-*/
-
             $db->close();
         }
 
-        /*
-
-        */
+        /**
+         * Viro::GenerateData()
+         * Generate the default data into the database, including the admin user, initial group, zone and content
+         */
         public static function GenerateData() {
             $db = new SQLite3('app/db/viro.db', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
 
@@ -205,37 +140,15 @@
             $db->query('INSERT INTO "articles" ("title", "u_id", "content", "a_hash", "created", "updated", "published")
                         VALUES ("Article title", "1", "Example article content.", "arthash", "' . $ct . '", "' . $ct . '", "0")');
 
-
-/*
-            # Admin user
-            $db->query('INSERT INTO "users" ("username", "email", "password", "read", "write", "users", "tools", "last_login", "active")
-                        VALUES ("admin", "cms@viro.app", "' . $adUser . '", "on", "on", "on", "on", "0", "1")');
-
-            # Generated group
-            $db->query('INSERT INTO "groups" ("g_name", "g_slug", "g_hash", "g_owner", "created")
-                        VALUES ("Main Group", "main-group", "grphash", "1", "0")');
-
-            # Generated zone
-            $db->query('INSERT INTO "zones" ("z_name", "z_slug", "z_hash", "g_hash", "z_owner", "created")
-                        VALUES ("Header Zone", "header-zone", "znehash", "grphash", "1", "0")');
-
-            # Generated content
-            $db->query('INSERT INTO "content" ("content", "c_hash", "z_hash", "g_hash", "created", "edit_by", "updated")
-                        VALUES ("Test Content", "conhash", "znehash", "grphash", "0", "1", "0")');
-
-            # Generated articles
-            $db->query('INSERT INTO "articles" ("title", "author", "content", "a_hash", "created", "updated")
-                        VALUES ("Test Title", "1", "Test content here", "arthash", "0", "0")');
-*/
-
             $db->exec('COMMIT');
 
             $db->close();
         }
 
-        /*
-
-        */
+        /**
+         * Viro::Content($content)
+         * Taking in the content hash, this function returns the content for that zone
+         */
         public static function Content($content) {
             $db = new SQLite3('app/db/viro.db', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
 
@@ -251,9 +164,10 @@
             echo $getContentRes['content'];
         }
 
-        /*
-
-        */
+        /**
+         * Viro::Permission($page)
+         * Taking in the page, this checks if the user has permissions to view the area
+         */
         public static function Permission($page) {
             $db = new SQLite3('app/db/viro.db', SQLITE3_OPEN_CREATE | SQLITE3_OPEN_READWRITE);
 
@@ -275,40 +189,44 @@
             }
         }
 
-        /*
-
-        */
+        /**
+         * Viro::Backup()
+         * Function to backup the SQLite database
+         */
         public static function Backup() {
             return;
         }
 
-        /*
-
-        */
+        /**
+         * Viro::Restore($id)
+         * Function to restore the SQLite database
+         */
         public static function Restore($id) {
             return;
         }
 
-        /*
-
-        */
+        /**
+         * Viro::Translate($string, $lang)
+         * Taking in the string to translate and the language file, this will replace $string with the translation
+         */
         public static function Translate($string, $lang) {
             echo $lang[$string];
         }
 
-        /*
-
-        */
+        /**
+         * Viro::Clean($string)
+         * This will remove special characters and double hyphens from $string
+         */
         public static function Clean($string) {
             $string = str_replace(' ', '-', $string);
             $string = preg_replace('/[^A-Za-z0-9\-]/', '', $string);
-         
             return preg_replace('/-+/', '-', $string);
-         }
+        }
 
-         /*
-
-        */
+        /**
+         * Viro::LoggedIn()
+         * Check logged in status by looking at the sessions
+         */
         public static function LoggedIn() {
             if(!isset($_SESSION['UserID']) || !isset($_SESSION['Username'])) {
 				return 0;
@@ -317,9 +235,10 @@
 			}
         }
 
-        /*
-
-        */
+        /**
+         * Viro::LoadView($view)
+         * Load a view by including the relevant php file, if not found then load the 404
+         */
         public static function LoadView($view) {
             if(file_exists('app/tpl/' . $view . '.php')) {
                 include 'app/tpl/' . $view . '.php';
@@ -328,23 +247,26 @@
             }
         }
 
-        /*
-
-        */
+        /**
+         * Viro::LoadPage($page)
+         * Load a page with a header redirect
+         */
         public static function LoadPage($page) {
             header("Location: ?page=" . $page);
         }
 
-        /*
-
-        */
+        /**
+         * Viro::Version()
+         * Return the current version of ViroCMS
+         */
         public static function Version() {
             return "v0.1-alpha";
         }
 
-        /*
-
-        */
+        /**
+         * Viro::CheckUpdate()
+         * Check if updates are available using Viro::Version() and the website
+         */
         public static function CheckUpdate() {
             # Get the current version
             $getVer = file_get_contents('https://viro.app/version.txt');
@@ -358,6 +280,18 @@
                 return true;
             }else{
                 return false;
+            }
+        }
+
+        /**
+         * Viro::Installed()
+         * Check the installation status
+         */
+        public static function Installed() {
+            if(!file_exists('app/db/viro.db')) {
+                return false;
+            }else{
+                return true;
             }
         }
     }
